a1997252a6e08837df2bfabb3e772d6b59da1965
Nicola Cimmino <nicola@nicolacimmino.com>
2013-11-04 11:14:30 +0200

    Cleaned up tracker code, fixed stability issues

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -19,13 +19,66 @@ import shelve
 import select
 from hash_ring import HashRing
 
+
+#
+# Send a command and get the reply.
+def sendCommand(ip, port, command):
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((ip, port))
+  s.send(command + "\n")
+  response = ""
+  timeout = False;
+  while not response.endswith("\n") and len(response) < 1024 and not timeout:
+   ready = select.select([s], [], [], 100)
+   if ready[0]:
+     data = s.recv(1024)
+     response += data
+   else:
+     response = ""
+     timeout = True
+  s.close()
+  return response[0:-1] # Remove trailing \n
+
+#
+# Wait a query. Bloks until a valid query is received.
+def waitCommand(s):
+ while True:
+  conn, addr = s.accept()
+  conn.setblocking(0)
+  query = ""
+  timeout = False;
+  while  not query.endswith("\n") and len(query) < 1024 and not timeout:
+   ready = select.select([conn], [], [], 100)
+   if ready[0]:
+    data = conn.recv(1024)
+    query += data
+   else:
+    conn.close()
+    timeout = True
+   if not timeout:
+    return conn, query[0:-1] # Remove trailing \n
+
+#
+# Send a response to a query previously received by waitCommand
+def sendResponse(conn, response):
+  conn.send(response + "\n")
+  conn.close()
+
+#
+# Gets the tracker URL, IP and port for a given URI
+def getTracker(uri):
+  trackeraddr = ring.get_node(uri)
+  trackeraddrtokens = trackeraddr.split(":")
+  return trackeraddr, trackeraddrtokens[0], int(trackeraddrtokens[1],10)
+
 # Expect the first parameter to be the port to listen to
 if len(sys.argv) != 3:
  print "Usage: tracker extip port"
  sys.exit(1)
 
-port = long(sys.argv[2],10)
-ownaddr = sys.argv[1] + ":" + sys.argv[2]
+ownport = long(sys.argv[2],10)
+ownaddr = sys.argv[1]
+ownuri = sys.argv[1] + ":" + sys.argv[2]
 
 #Get the trackers roster from file
 trackers_urls = open("data/trackers_roster").read().splitlines()
@@ -44,73 +97,52 @@ nodeslocation = shelve.open("data/nodes_location_"+ownaddr)
 ring = HashRing(trackers,3)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', port))
+s.bind(('', ownport))
 s.listen(1)
 try:
- while 1:
-  conn, addr = s.accept()
-  conn.setblocking(0)
-  query = ""
-  while  not query.endswith("\n") and len(query) < 1024:
-   ready = select.select([conn], [], [], 100)
-   if ready[0]:
-    data = conn.recv(1024)
-    query += data
-   else:
-    conn.close()
-    continue
-
-  print ">", query
-
-  response = "UNKNOWN\n"
+ while True:
+  conn, query = waitCommand(s)
+  print ownport, ">", query
+  response = "UNKNOWN"
 
   # Tokenize on white spaces 
   querytokens = query.split();
 
-  # Client wants to find the proper tracker for a given UID
+  # Client wants to find the proper tracker for a given URL
   if len(querytokens) == 2 and querytokens[0] == "TRK":
-   response = ring.get_node(query[4:])
+   response = ring.get_node(querytokens[1])
 
   # Client wants to register with the tracker
   if len(querytokens) == 3 and  querytokens[0] == "REG":
-   uid=querytokens[1]
-   url=querytokens[2]
-   trackeraddr = ring.get_node(uid)
-   trackeraddrtokens = trackeraddr.split(":")
+   uri=querytokens[1]
+   url=querytokens[2]   
+   trackerurl, trackerip, trackerport = getTracker(uri)
 
-   if trackeraddr != ownaddr:
+   if trackerurl != ownuri:
     # Not our node, rely the query
-    ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ts.connect((trackeraddrtokens[0], long(trackeraddrtokens[1],10)))
-    ts.send(query)
-    response = ts.recv(1024)
-    ts.close()
+    response = sendCommand(trackerip, trackerport, query)
    else:
-    nodeslocation[uid]=url
-    response = "OK\n"
+    # Cache the URL of this URI
+    nodeslocation[uri]=url
+    nodeslocation.sync()
+    response = "OK"
  
   # Client wants to locate a node
   if len(querytokens) == 2 and  querytokens[0] == "LOC":
-   uid=querytokens[1]
-   trackeraddr = ring.get_node(uid)
-   trackeraddrtokens = trackeraddr.split(":")
+   uri=querytokens[1]
+   trackerurl, trackerip, trackerport = getTracker(uri)
    
-   if trackeraddr != ownaddr:
+   if trackerurl != ownuri:
     # Not our node, rely the query
-    ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ts.connect((trackeraddrtokens[0], long(trackeraddrtokens[1],10)))
-    ts.send(query)
-    response = ts.recv(1024)
-    ts.close()    
+    response = sendCommand(trackerip, trackerport, query)
    else:
-    if nodeslocation.has_key(uid):  
-     response = nodeslocation[uid]
+    if nodeslocation.has_key(uri):  
+     response = nodeslocation[uri]
     else:
-     response = "NOTHERE\n"
+     response = "NOTHERE"
 
-  print "<", response
-  conn.send(response)  
-  conn.close()
+  print ownport, "<", response
+  sendResponse(conn, response)  
 except:
   print "Terminating."
   s.close()
