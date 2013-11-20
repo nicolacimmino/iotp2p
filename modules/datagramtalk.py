fb3d91570dcfdfea3b2c7bd3127a14f3cff4d6a8
Nicola Cimmino <nicola@nicolacimmino.com>
2013-11-20 20:54:40 +0200

    Changed according to DatagramTalk  Version: 0.2 Edited By: Nicola Date: 20.11.2013

--- modules/datagramtalk.py
+++ modules/datagramtalk.py
@@ -20,46 +20,91 @@ import select
 import sys
 
 #
-# A Datagram message conveniently split into its tokens
+# A Datagram message conveniently split into its parts and with the 
+#  ability to reasseamble a raw message starting from the parts.
 class datagramTalkMessage:
-
-  # Constrocut the object starting from a string message
-  def __init__ ( self, message ):
-    message_tokens = message.split(' ')
-
-    # Store the statement
-    if len( message_tokens ) > 0:
-      self.statement = message_tokens[0]
-
-    # Store the arguments
-    if len( message_tokens ) > 1:
-      self.arguments =  message_tokens[1:]
-
-    # Store the raw message
-    self.raw = message
-
+ 
   # The statement part of the message.
   statement = ""
 
-  # The arguments of the statement.
-  arguments = []
+  # The higer level protocol
+  protocol = ""
+  
+  # The higher level protocol version
+  protocol_version = ""
+  
+  # The parameters of the statement.
+  parameters = {}
 
   # The raw message as it was received.  
   raw = ""
+  
+  # Constrocut the object starting from a string representing the all message
+  def __init__ ( self, message ):
+    
+    try:
+      if not message == "":
+        # Tolerate \r\n, split lines on \n
+        message_lines = message.translate(None, 'r').split('\n')
 
+        # The first line is:
+        # protocol version statement
+        tokens = message_lines[0].split( ' ' )
+        self.protocol = tokens[0]
+        self.protocol_version = tokens[1]
+        self.statement = tokens[2]
+        
+        # Find all parameters
+        for message_line in message_lines:
+          if '=' in message_line:
+            tokens = message_line.split( '=' )
+            self.parameters[tokens[0]] = tokens[1]
+    except:
+       # Something went wrong just store the raw message
+       print "Exception while parsing dtalk message"
+       
+    # Store the raw message
+    self.raw = message
+  
+  def toRaw( self ):
+    self.raw = ""
+    
+    # First line has protocol, version and statement
+    self.raw += self.protocol + " " + self.protocol_version + " " + self.statement + "\n"
+    
+    # Following lines are the params
+    for key in self.parameters.keys():
+      self.raw += key + "=" + self.parameters[key] + "\n"
+
+    # Close the message with an empty line
+    self.raw += "\n"
+
+    # Als return the raw message
+    return self.raw
+    
 class datagramtalk:
 
   # When set causes the listening thread to terminate.
   __server_term = False
 
-  # Receiver timeout in mS, max time waited for data once a connection is extablished.
-  __rx_timeout = 10000
+  # Timeout in seconds for waiting from data from a requester
+  # See DatagramTalk section Timers Ta.
+  __timer_Ta_timeout = 10
+
+  # Timeout in seconds for waiting for a response to a request
+  # See DatagramTalk section Timers Tb.
+  __timer_Tb_timeout = 10
+  
+  # Receiver buffer size, no message can be larger than this.
+  __rx_buffer_size = 1024
+  
+  # Default constructor, doesn't listen only allows to send messages. 
+  def __init__( self ):
+    print "ok"
 
   # This constructor initializes the server and listens for messages.
   def __init__( self, ip, port, msg_hook ):
-
-    if ip == None:
-      return
+    # Nothing to do
 
     # Start a thread listeining for incoming messages.
     thread = Thread( target = self.startServer, args = ( ip, port, msg_hook ) )
@@ -69,20 +114,20 @@ class datagramtalk:
   def stopListening( self ):
     self.__server_term = True
 
-  # Send a message and get the reply.
-  def sendMessage( self, ip, port, command ):
+  # Sends a datagram and gets the response datagram.
+  def sendDatagram( self, ip, port, datagram ):
     try:
       s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
       s.connect( ( ip, port ) )
-      s.send( command + "\n" )
+      s.send( datagram.toRaw() )
       response = ""
       timeout = False;
-      while not response.endswith( "\n" ) and len( response ) < 1024 and not timeout:
+      while not (response.endswith("\n\n") or response.endswith("\r\n\r\n")) and len( response ) < __rx_buffer_size and not timeout:
        
        # Allow data to come in chunks but don't wait more than rx_timeout
-       ready = select.select( [s], [], [], self.__rx_timeout / 1000 )
+       ready = select.select( [s], [], [], self.__timer_Tb_timeout )
        if ready[0]:
-         data = s.recv(1024)
+         data = s.recv( __rx_buffer_size )
          response += data
        else:
          response = ""
@@ -90,7 +135,7 @@ class datagramtalk:
 
       # Whatever happened we end communication and return response if any
       s.close()
-      return datagramTalkMessage(response[0:-1]) # Remove trailing \n
+      return datagramTalkMessage( response )
 
     except:
       # Something went wrong, convention is to return empty string.
@@ -111,8 +156,8 @@ class datagramtalk:
         client_socket, address = server_socket.accept()
         query = ""
         timeout = False;
-        while  not query.endswith("\n") and len(query) < 1024 and not timeout:
-         ready = select.select( [client_socket], [], [], self.__rx_timeout / 1000 )
+        while  not (query.endswith("\n\n") or query.endswith("\r\n\r\n")) and len(query) < 1024 and not timeout:
+         ready = select.select( [client_socket], [], [], self.__timer_Ta_timeout )
          if ready[0]:
           data = client_socket.recv(1024)
           query += data
@@ -120,9 +165,8 @@ class datagramtalk:
           client_socket.close()      
           timeout = True
          if not timeout:
-          query = query.translate(None, '\r\n')
-          response = msg_hook(datagramTalkMessage(query)) # Process the command and get the response
-          client_socket.send(response + "\n")
+          response = msg_hook( datagramTalkMessage( query ) ) # Process the request and get the response
+          client_socket.send(response)
           client_socket.close()
           break;
     except:
